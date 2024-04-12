888a99a288ffe78018dac9c86967e053b18dfc54
Liz Bayardelle <lizbayardelle@gmail.com>
2024-04-12 14:21:26 -0700

    Testing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,19 +1,8 @@
-import { Application } from "@hotwired/stimulus";
-const application = Application.start();
+import "./controllers"
+import * as bootstrap from "bootstrap"
 
-// Assuming your controllers are in a directory named "controllers"
-// and webpack is correctly set up to import all files from that directory
-// Adjust the path as needed for your project structure
-const controllers = require.context("./controllers", true, /\.js$/);
-application.load(controllers.keys().map(controllers));
-
-import { Turbo } from "@hotwired/turbo-rails";
-import * as bootstrap from 'bootstrap';
-import Swal from 'sweetalert2';
-
-// If these are custom files, ensure they're correctly set up and exported
-import "./src/jquery";
-import "./src/jquery-ui";
+import "./src/jquery"
+import "./src/jquery-ui"
 
 // Initialize any jQuery or other JS library setups
 $(function() {
